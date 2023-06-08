ebee19be8cd080439599186755b1f975afe0d539
RadouaneGaroiaz <garoiaz.radouane@gmail.com>
2023-06-08 15:26:22 +0200

    change placememnt

---
+++ src/main/resources/front/src/constants/tables.ts
@@ -0,0 +1,128 @@
+export const customersHeader = [
+  "firstname",
+  "lastname",
+  "email",
+  "photo",
+  "created_at",
+  "active",
+
+];
+export const customers = [
+  {
+    firstname: "John",
+    lastname: "Doe",
+    email: "john@doe.com",
+    photo: "",
+    created_at: "2021-07-01",
+    active: true,
+  },
+  {
+    firstname: "Jane",
+    lastname: "Doe",
+    email: "jane@doa@gmail.com",
+    photo: "",
+    created_at: "2021-07-01",
+    active: true,
+  },
+  {
+    firstname: "John",
+    lastname: "Doe",
+    email: "",
+    photo: "",
+    created_at: "2021-07-01",
+    active: true, 
+  },
+  {
+    firstname: "jack",
+    lastname: "Doe",
+    email: "jack@doa@gmail.com",
+    photo: "",
+    created_at: "2021-07-01",
+    active: true, 
+  },
+  {
+    firstname: "momo",
+    lastname: "kok",
+    email: "mom@kok.gg",
+    photo: "",
+    created_at: "2021-07-01",
+    active: true, 
+  },
+  {
+    firstname: "tata",
+    lastname: "fofo",
+    email: "fofo@tat.gg",
+    photo: "",
+    created_at: "2021-07-01",
+    active: true, 
+  },
+
+];
+
+export const productsHeader = [
+  "name",
+  "code",
+  "description",
+  "category",
+  "price",
+ 
+];
+export const products = [
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  },
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  },
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  },
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  },
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  },
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  },
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  },
+  {
+    name: "NUBWO G06",
+    code: "NUBWO G06",
+    description: "NUBWO G06",
+    category: "digital",
+    price: "$120.00",
+  }
+
+];

0cca48bb73774ce11234de0c4b6bb488460634a0
Amine Alami <alamia502@gmail.com>
2023-06-08 10:27:40 +0200

    init commit

---
+++ src/main/java/com/hitema/projetjavaapi/ProjetJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.hitema.projetjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJavaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetJavaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hitema/projetjavaapi/ProjetJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitema.projetjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJavaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
